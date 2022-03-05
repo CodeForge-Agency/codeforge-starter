de8d26194f67ff2798a01976daaaa92de97ec4a9
David Elster <davidandtheweb@gmail.com>
2022-03-05 12:49:23 -0500

    :heavy_plus_sign: Tailwind CSS / :heavy_minus_sign: TypeScript

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
+// Official Documentation: https://tailwindcss.com/docs/configuration
+const colors = require('tailwindcss/colors')
+
+// Remove warnings about deprecated colors in `tailwindcss/colors`:
+// delete colors['lightBlue']
+// delete colors['warmGray']
+// delete colors['trueGray']
+// delete colors['coolGray']
+// delete colors['blueGray']
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}'
   ],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      colors: {
+        ...colors
+      },
+      // fontFamily: {
+      //   mono:  [],
+      //   sans:  [],
+      //   serif: [],
+      // }
+    }
   },
-  plugins: [],
+  variants: {},
+  plugins: []
 }
